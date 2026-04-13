209e87b3544252cab5bd213a1d50d0f008e097e4
Silbet <gs1237@kookmin.ac.kr>
2026-04-14 02:21:46 +0900

    chore: Docker MySQL 로컬 개발 환경 추가

---
+++ init-data.sql
@@ -0,0 +1,12 @@
+SET NAMES utf8mb4;
+INSERT INTO tracks (title, artist, album, duration, recorded_at, audio_url, cover_url, members)
+VALUES (
+  '청춘만화',
+  '이무진',
+  '만화 (滿花)',
+  0,
+  '2025-01-31',
+  'https://your-s3-bucket.s3.ap-northeast-2.amazonaws.com/audio/youth-comic.mp3',
+  NULL,
+  '[{"name": "김민주", "part": "보컬1"}, {"name": "이석형", "part": "보컬2"}, {"name": "송태관", "part": "기타1"}, {"name": "황겸", "part": "기타2"}, {"name": "김준범", "part": "건반"}, {"name": "박다은", "part": "베이스"}, {"name": "김민수", "part": "드럼"}]'
+);
